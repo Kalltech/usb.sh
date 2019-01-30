617806eb9bd1db1fcbc7b37b8fd495a2d00d9d96
Kall <infra@kalltech.net>
2019-01-30 11:52:55 +0100

    Update usb.sh

--- usb.sh
+++ usb.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+sleep 1
 # Fetching usbs location and putting them in array
 locIterator="0"
 locArr=()
+df >df.txt
 while read row
 do
-    loc=$( echo $row | grep -P '/media/.*?$' -o )
+    loc=$( echo $row | grep /share/external/ | awk '{print $6}' )
     if [ "$loc" != "" ]; then
 		locArr[ $locIterator ]="$loc"
 		(( locIterator++ ))
     fi    
-done < <(df)
+done < df.txt
 
 # Fetching usbs serial numbers and putting them in array
 snIterator="0"
 snArr=()
+cat /proc/scsi/usb-storage/* >usb.txt
 while read row
 do
-    sn=$( echo $row | grep -P 'Serial Number:.*?$' -o )
+    sn=$( echo $row | grep 'Serial Number:' | sed -r 's/^Serial Number://' )
 	if [ "$sn" != "" ]; then 
 		snArr[ $snIterator ]="${sn:15}"  
 		(( snIterator++ ))
 	fi	
-done < <(cat /proc/scsi/usb-storage/*)
+done < usb.txt
 
 # Generating Output by looping through both arrays
 output=""
@@ -31,6 +34,6 @@ do
 	if [ "$i" != "0"  ]; then
 		output="$output|"
 	fi
-	output="$output${locArr[$i]}: ${snArr[$i]}"
+	output="$output${locArr[$i]}"
 done
 echo $output
